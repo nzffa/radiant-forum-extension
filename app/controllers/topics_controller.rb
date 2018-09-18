052d219e83d79c95c5fa3300645871de7a896e07
jomz <hi@monkeypatch.be>
2018-09-18 21:51:17 +0200

    Revert order of posts in topics (and un-paginate them)

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,5 +1,5 @@
 class TopicsController < ForumBaseController
-  
+
   def index
     @topics = Topic.visible_to(current_reader).bydate.paginate(pagination_parameters)
     render_page_or_feed
@@ -8,7 +8,8 @@ class TopicsController < ForumBaseController
   def show
     @topic = Topic.visible_to(current_reader).find(params[:id])
     @forum = @topic.forum
-    @posts = @topic.replies.paginate(pagination_parameters)
+    @posts = @topic.replies.reverse
+    @posts.unshift(@posts.pop) # because the oldest post is the topic, the others are rendered new to old 
     render_page_or_feed
   end
 
