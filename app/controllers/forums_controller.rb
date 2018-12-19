531c544a2500f6daba4bfdfa76d91a7abd7c90c2
Benny Degezelle <hi@monkeypatch.be>
2018-12-19 13:40:00 +0100

    Improve error handling when a reader does not have access to a forum; render AccessDenied i.o. RecordNotFound

--- app/controllers/forums_controller.rb
+++ app/controllers/forums_controller.rb
@@ -6,7 +6,11 @@ class ForumsController < ForumBaseController
   end
 
   def show
-    @forum = Forum.visible_to(current_reader).find(params[:id])
+    begin
+      @forum = Forum.visible_to(current_reader).find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      raise ReaderError::AccessDenied, "The forum that you tried to reach is not public, and your account does not have access to it."
+    end
     @topics = @forum.topics.stickyfirst.paginate(pagination_parameters.merge(:per_page => Radiant.config['forum.posts_per_page']))
     render_page_or_feed
   end
